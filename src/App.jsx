2ea8b42e83c25c144ef3c8e50d78cc4c57c5e4e6
Andreas Papakostas <andreaspkst@gmail.com>
2024-02-29 12:32:56 +0200

    Created Structure and Layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import styles from './style';
 
-function App() {
-  return (
-    <div>App</div>
-  )
-}
+// Smart Import from Components - Check Components/index.js
+import { Billing, Business, CardDeal, Clients, CTA,
+  Footer, Hero, Navbar, Stats, Testimonials } from './components'
+
+
+const App = () => (
+    <div className='bg-primary w-full overflow-hidden'>
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Navbar />
+
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`bg-primary ${styles.flexStart}`}>
+          <Hero />
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`bg-primary ${styles.boxWidth}`}>
+          <Stats />
+          <Business />
+          <Billing />
+          <CardDeal />
+          <Testimonials />
+          <Clients /> 
+          <CTA /> 
+          <Footer /> 
+        </div>
+      </div>
+
+    </div>
+  );
 
 export default App
